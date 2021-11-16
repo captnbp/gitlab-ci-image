c859c575da575142eebb9f98c0c0cb504dc638cc
Benoit Pourre <benoit.pourre@gmail.com>
2021-11-16 21:50:38 +0100

    Upgrade tools

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,6 @@
-FROM golang:1.14 AS tfplantool
-
-ENV BASE="hashicorp/terraform:0.14.1"
-ENV TFPLANTOOL="v0.1.0"
-
-WORKDIR /tfplantool
-
-RUN git clone --branch $TFPLANTOOL --depth 1 https://gitlab.com/mattkasa/tfplantool.git .
-RUN sed -i -e "/github\.com\/hashicorp\/terraform/s/ v.*\$/ v$(echo "$BASE" | sed -e "s/^.*://")/" go.mod
-RUN go get -d -v ./...
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o tfplantool .
-
 FROM    ubuntu:20.04
 
 ENV DEBIAN_FRONTEND noninteractive 
-
-COPY	--from=tfplantool /tfplantool/tfplantool /usr/bin/tfplantool
 
 COPY    ./install_tools.sh /usr/local/bin/install_tools.sh
 
--- install_tools.sh
+++ install_tools.sh
@@ -16,7 +16,7 @@ echo "Install helm"
 HELM_INSTALL_DIR="/usr/local/bin"
 ARCH="amd64"
 latest_release_url="https://github.com/helm/helm/releases"
-TAG=$(curl -Ls $latest_release_url | grep 'href="/helm/helm/releases/tag/v3.' | head -n 1 | cut -d '"' -f 2 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}')
+TAG=$(curl -Ls $latest_release_url | grep 'href="/helm/helm/releases/tag/v3.' | head -n 1 | cut -d '"' -f 6 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}')
 OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
 HELM_DIST="helm-$TAG-$OS-$ARCH.tar.gz"
 DOWNLOAD_URL="https://get.helm.sh/$HELM_DIST"
@@ -53,7 +53,7 @@ echo "helm installed into $HELM_INSTALL_DIR/helm"
 
 echo "Install Packer"
 latest_release_url="https://github.com/hashicorp/packer/releases"
-TAG=$(curl -Ls $latest_release_url | grep 'href="/hashicorp/packer/releases/tag/v.' | grep -v beta | grep -v rc | head -n 1 | cut -d '"' -f 2 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}' | cut -d 'v' -f2)
+TAG=$(curl -Ls $latest_release_url | grep 'href="/hashicorp/packer/releases/tag/v.' | grep -v beta | grep -v rc | head -n 1 | cut -d '"' -f 6 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}' | cut -d 'v' -f2)
 wget "https://releases.hashicorp.com/packer/${TAG}/packer_${TAG}_linux_amd64.zip" -O /tmp/packer.zip >/dev/null
 unzip /tmp/packer.zip >/dev/null
 mv -f /tmp/packer /usr/local/bin/packer
@@ -61,7 +61,7 @@ rm /tmp/packer.zip
 
 echo "Install Terraform"
 latest_release_url="https://github.com/hashicorp/terraform/releases"
-TAG=$(curl -Ls $latest_release_url | grep 'href="/hashicorp/terraform/releases/tag/v.' | grep -v beta | grep -v rc | head -n 1 | cut -d '"' -f 2 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}' | cut -d 'v' -f2)
+TAG=$(curl -Ls $latest_release_url | grep 'href="/hashicorp/terraform/releases/tag/v.' | grep -v beta | grep -v rc | head -n 1 | cut -d '"' -f 6 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}' | cut -d 'v' -f2)
 wget "https://releases.hashicorp.com/terraform/${TAG}/terraform_${TAG}_linux_amd64.zip" -O /tmp/terraform.zip >/dev/null
 unzip terraform.zip >/dev/null
 mv -f /tmp/terraform /usr/local/bin/terraform
@@ -70,7 +70,7 @@ rm /tmp/terraform.zip
 
 echo "Install Vault"
 latest_release_url="https://github.com/hashicorp/vault/releases"
-TAG=$(curl -Ls $latest_release_url | grep 'href="/hashicorp/vault/releases/tag/v.' | grep -v beta | grep -v rc | head -n 1 | cut -d '"' -f 2 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}' | cut -d 'v' -f2)
+TAG=$(curl -Ls $latest_release_url | grep 'href="/hashicorp/vault/releases/tag/v.' | grep -v beta | grep -v rc | head -n 1 | cut -d '"' -f 6 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}' | cut -d 'v' -f2)
 wget "https://releases.hashicorp.com/vault/${TAG}/vault_${TAG}_linux_amd64.zip" -O /tmp/vault.zip >/dev/null
 unzip /tmp/vault.zip >/dev/null
 mv -f /tmp/vault /usr/local/bin/vault
@@ -83,7 +83,7 @@ chmod 755 /usr/local/bin/mc
 
 echo "Install Restic cli"
 latest_release_url="https://github.com/restic/restic/releases/"
-TAG=$(curl -Ls $latest_release_url | grep 'href="/restic/restic/releases/tag/v.' | grep -v beta | grep -v rc | head -n 1 | cut -d '"' -f 2 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}' | cut -d 'v' -f2)
+TAG=$(curl -Ls $latest_release_url | grep 'href="/restic/restic/releases/tag/v.' | grep -v beta | grep -v rc | head -n 1 | cut -d '"' -f 6 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}' | cut -d 'v' -f2)
 wget "https://github.com/restic/restic/releases/download/v${TAG}/restic_${TAG}_linux_amd64.bz2" -O /tmp/restic.bz2 >/dev/null
 bzip2 -d /tmp/restic.bz2
 mv /tmp/restic /usr/local/bin/restic
@@ -98,7 +98,7 @@ chmod 755 /usr/local/bin/scw
 
 echo "Install Hadolint"
 latest_release_url="https://github.com/hadolint/hadolint/releases"
-TAG=$(curl -Ls $latest_release_url | grep 'href="/hadolint/hadolint/releases/tag/v.' | grep -v rc | head -n 1 | cut -d '"' -f 2 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}')
+TAG=$(curl -Ls $latest_release_url | grep 'href="/hadolint/hadolint/releases/tag/v.' | grep -v rc | head -n 1 | cut -d '"' -f 6 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}')
 wget "https://github.com/hadolint/hadolint/releases/download/${TAG}/hadolint-Linux-x86_64" -O /usr/local/bin/hadolint >/dev/null
 chmod 755 /usr/local/bin/hadolint
 

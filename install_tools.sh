50fa4d43da9d84e38afe3085f380bb4ae5d28526
Benoit Pourre <benoit.pourre@gmail.com>
2023-11-01 18:35:32 +0100

    Improve install script
    Signed-off-by: Benoit Pourre <benoit.pourre@gmail.com>

--- install_tools.sh
+++ install_tools.sh
@@ -6,8 +6,9 @@ DEBIAN_FRONTEND=noninteractive
 
 echo "Install tools"
 apt-get update >/dev/null
-apt-get install -y --no-install-recommends vim pwgen jq wget curl unzip software-properties-common gpg gettext ca-certificates openssh-client git bzip2 skopeo shellcheck golang
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | tee /usr/share/keyrings/trivy.gpg > /dev/null
+apt-get dist-upgrade -y
+apt-get install -y --no-install-recommends procps lsb-release vim pwgen jq wget curl unzip software-properties-common gpg gettext ca-certificates openssh-client git bzip2
+curl -sL --fail --show-error https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | tee /usr/share/keyrings/trivy.gpg > /dev/null
 echo "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb buster main" | tee -a /etc/apt/sources.list.d/trivy.list
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker.gpg
 chmod a+r /usr/share/keyrings/docker.gpg
@@ -17,8 +18,11 @@ echo \
   $(source /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update >/dev/null
-apt-get install -y --no-install-recommends trivy docker-ce-cli docker-buildx-plugin docker-compose-plugin
-apt-get dist-upgrade -y
+apt-get install -y --no-install-recommends skopeo htop netcat-openbsd uuid-runtime dnsutils exa fd-find trivy iproute2 nmap iperf3 docker-ce-cli docker-buildx-plugin docker-compose-plugin golang shellcheck python3-pip python3-setuptools python3-ldap python3-docker python3-venv twine python3-psycopg2 gcc python3-dev
+
+# https://wiki.debian.org/Locale#Manually
+sed -i "s/# en_US.UTF-8/en_US.UTF-8/" /etc/locale.gen 
+locale-gen
 
 # For AMD64 / x86_64
 [ "$(uname -m)" = x86_64 ] && ARCH="amd64"
@@ -26,29 +30,34 @@ apt-get dist-upgrade -y
 [ "$(uname -m)" = aarch64 ] && ARCH="arm64"
 OS=$(uname |tr '[:upper:]' '[:lower:]')
 
+echo "Install Ansible and ansible-modules-hashivault"
+# https://www.linuxuprising.com/2023/03/next-debianubuntu-releases-will-likely.html?m=1
+export PIP_BREAK_SYSTEM_PACKAGES=1
+pip3 install --no-cache-dir ansible ansible-modules-hashivault openshift passlib hvac elasticsearch virtualenv ipykernel checkov opensearch-py
+
+ln -s "$(which fdfind)" /usr/local/bin/fd
+
 echo "Install kubectl"
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/${OS}/${ARCH}/kubectl" >/dev/null
-chmod +x /tmp/kubectl
-mv -f /tmp/kubectl /usr/local/bin/kubectl
+curl -sL --fail --show-error "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/${OS}/${ARCH}/kubectl" -o /usr/local/bin/kubectl
+chmod +x /usr/local/bin/kubectl
 
 echo "Install helm"
-HELM_VERSION=$(curl -Ls https://api.github.com/repos/helm/helm/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+HELM_VERSION=$(curl -sL https://api.github.com/repos/helm/helm/releases/latest | jq -r .tag_name)
 HELM_DIST="helm-$HELM_VERSION-$OS-$ARCH.tar.gz"
 DOWNLOAD_URL="https://get.helm.sh/$HELM_DIST"
 CHECKSUM_URL="$DOWNLOAD_URL.sha256"
 HELM_TMP_ROOT="$(mktemp -dt helm-installer-XXXXXX)"
 HELM_TMP_FILE="$HELM_TMP_ROOT/$HELM_DIST"
 HELM_SUM_FILE="$HELM_TMP_ROOT/$HELM_DIST.sha256"
-echo "Downloading $DOWNLOAD_URL"
 if type "curl" > /dev/null; then
   curl -SsL "$CHECKSUM_URL" -o "$HELM_SUM_FILE"
 elif type "wget" > /dev/null; then
-  wget -q -O "$HELM_SUM_FILE" "$CHECKSUM_URL"
+  curl -sL --fail --show-error -o "$HELM_SUM_FILE" "$CHECKSUM_URL"
 fi
 if type "curl" > /dev/null; then
   curl -SsL "$DOWNLOAD_URL" -o "$HELM_TMP_FILE"
 elif type "wget" > /dev/null; then
-  wget -q -O "$HELM_TMP_FILE" "$DOWNLOAD_URL"
+  curl -sL --fail --show-error -o "$HELM_TMP_FILE" "$DOWNLOAD_URL"
 fi
 # installFile verifies the SHA256 for the file, then unpacks and
 # installs it.
@@ -65,77 +74,77 @@ HELM_TMP_BIN="$HELM_TMP/$OS-$ARCH/helm"
 cp "$HELM_TMP_BIN" "/usr/local/bin"
 
 echo "Install Packer"
-PACKER_VERSION=$(curl -sL "https://api.github.com/repos/hashicorp/packer/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-wget "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_${OS}_${ARCH}.zip" -O /tmp/packer.zip >/dev/null
+PACKER_VERSION=$(curl -sL "https://api.github.com/repos/hashicorp/packer/releases/latest" | jq -r .tag_name | sed -E 's/v(.*)/\1/')
+curl -sL --fail --show-error "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_${OS}_${ARCH}.zip" -o /tmp/packer.zip
 unzip /tmp/packer.zip >/dev/null
 mv -f /tmp/packer /usr/local/bin/packer
 rm /tmp/packer.zip
 
 echo "Install Terraform"
-TERRAFORM_VERSION=$(curl -sL "https://api.github.com/repos/hashicorp/terraform/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-wget "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip" -O /tmp/terraform.zip >/dev/null
+TERRAFORM_VERSION=$(curl -sL "https://api.github.com/repos/hashicorp/terraform/releases/latest" | jq -r .tag_name | sed -E 's/v(.*)/\1/')
+curl -sL --fail --show-error "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip" -o /tmp/terraform.zip
 unzip terraform.zip >/dev/null
 mv -f /tmp/terraform /usr/local/bin/terraform
 chown 0755 /usr/local/bin/terraform
 rm /tmp/terraform.zip
 
 echo "Install Vault"
-VAULT_VERSION=$(curl -sL "https://api.github.com/repos/hashicorp/vault/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-wget "https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_${OS}_${ARCH}.zip" -O /tmp/vault.zip >/dev/null
+VAULT_VERSION=$(curl -sL "https://api.github.com/repos/hashicorp/vault/releases/latest" | jq -r .tag_name | sed -E 's/v(.*)/\1/')
+curl -sL --fail --show-error "https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_${OS}_${ARCH}.zip" -o /tmp/vault.zip
 unzip /tmp/vault.zip >/dev/null
 mv -f /tmp/vault /usr/local/bin/vault
 chown 0755 /usr/local/bin/vault
 rm /tmp/vault.zip
 
 echo "Install Minio mc client"
-wget "https://dl.min.io/client/mc/release/${OS}-${ARCH}/mc" -O /usr/local/bin/mc >/dev/null
+curl -sL --fail --show-error "https://dl.min.io/client/mc/release/${OS}-${ARCH}/mc" -o /usr/local/bin/mc
 chmod 0755 /usr/local/bin/mc
 
 echo "Install Restic cli"
-RESTIC_VERSION=$(curl -sL "https://api.github.com/repos/restic/restic/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-wget "https://github.com/restic/restic/releases/download/v${RESTIC_VERSION}/restic_${RESTIC_VERSION}_${OS}_${ARCH}.bz2" -O /tmp/restic.bz2 >/dev/null
+RESTIC_VERSION=$(curl -sL "https://api.github.com/repos/restic/restic/releases/latest" | jq -r .tag_name | sed -E 's/v(.*)/\1/')
+curl -sL --fail --show-error "https://github.com/restic/restic/releases/download/v${RESTIC_VERSION}/restic_${RESTIC_VERSION}_${OS}_${ARCH}.bz2" -o /tmp/restic.bz2
 bzip2 -d /tmp/restic.bz2
 mv /tmp/restic /usr/local/bin/restic
 chmod 0755 /usr/local/bin/restic
 
 echo "Install Scaleway scw cli"
-SCW_VERSION=$(curl -sL "https://api.github.com/repos/scaleway/scaleway-cli/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-wget "https://github.com/scaleway/scaleway-cli/releases/download/v${SCW_VERSION}/scaleway-cli_${SCW_VERSION}_${OS}_${ARCH}" -O /usr/local/bin/scw >/dev/null
+SCW_VERSION=$(curl -sL "https://api.github.com/repos/scaleway/scaleway-cli/releases/latest" | jq -r .tag_name | sed -E 's/v(.*)/\1/')
+curl -sL --fail --show-error "https://github.com/scaleway/scaleway-cli/releases/download/v${SCW_VERSION}/scaleway-cli_${SCW_VERSION}_${OS}_${ARCH}" -o /usr/local/bin/scw
 chmod 0755 /usr/local/bin/scw
 
 echo "Install Hadolint"
-HADOLINT_VERSION=$(curl -sL "https://api.github.com/repos/hadolint/hadolint/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-[ "$(uname -m)" = x86_64 ] && wget "https://github.com/hadolint/hadolint/releases/download/${HADOLINT_VERSION}/hadolint-Linux-x86_64" -O /usr/local/bin/hadolint >/dev/null
-[ "$(uname -m)" = aarch64 ] && wget "https://github.com/hadolint/hadolint/releases/download/${HADOLINT_VERSION}/hadolint-Linux-arm64" -O /usr/local/bin/hadolint >/dev/null
+HADOLINT_VERSION=$(curl -sL "https://api.github.com/repos/hadolint/hadolint/releases/latest" | jq -r .tag_name)
+[ "$(uname -m)" = x86_64 ] && curl -sL --fail --show-error "https://github.com/hadolint/hadolint/releases/download/${HADOLINT_VERSION}/hadolint-Linux-x86_64" -o /usr/local/bin/hadolint
+[ "$(uname -m)" = aarch64 ] && curl -sL --fail --show-error "https://github.com/hadolint/hadolint/releases/download/${HADOLINT_VERSION}/hadolint-Linux-arm64" -o /usr/local/bin/hadolint
 chmod 0755 /usr/local/bin/hadolint
 
 echo "Install cosign"
-curl -O -L "https://github.com/sigstore/cosign/releases/latest/download/cosign-${OS}-${ARCH}"
+curl -sLO --fail --show-error "https://github.com/sigstore/cosign/releases/latest/download/cosign-${OS}-${ARCH}"
 mv "cosign-${OS}-${ARCH}" /usr/local/bin/cosign
 chmod +x /usr/local/bin/cosign
 
 echo "Install dive"
-DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-curl -OL "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_${OS}_${ARCH}.deb"
+DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | jq -r .tag_name | sed -E 's/v(.*)/\1/')
+curl -sLO --fail --show-error "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_${OS}_${ARCH}.deb"
 apt install "./dive_${DIVE_VERSION}_${OS}_${ARCH}.deb"
 rm "./dive_${DIVE_VERSION}_${OS}_${ARCH}.deb"
 
 echo "Install oras"
-ORAS_VERSION=$(curl -sL https://api.github.com/repos/oras-project/oras/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-curl -LO "https://github.com/oras-project/oras/releases/download/v${ORAS_VERSION}/oras_${ORAS_VERSION}_${OS}_${ARCH}.tar.gz"
+ORAS_VERSION=$(curl -sL https://api.github.com/repos/oras-project/oras/releases/latest | jq -r .tag_name | sed -E 's/v(.*)/\1/')
+curl -sLO --fail --show-error "https://github.com/oras-project/oras/releases/download/v${ORAS_VERSION}/oras_${ORAS_VERSION}_${OS}_${ARCH}.tar.gz"
 mkdir -p oras-install/
 tar -zxf "oras_${ORAS_VERSION}_${OS}_${ARCH}.tar.gz" -C oras-install/
 mv oras-install/oras /usr/local/bin/
 rm -rf "oras_${ORAS_VERSION}_${OS}_${ARCH}.tar.gz" oras-install/
 
 echo "Install kind"
-KIND_VERSION=$(curl https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-curl -Lo /usr/local/bin/kind "https://kind.sigs.k8s.io/dl/v${KIND_VERSION}/kind-${OS}-${ARCH}"
+KIND_VERSION=$(curl -sL https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq -r .tag_name | sed -E 's/v(.*)/\1/')
+curl -sL --fail --show-error -o /usr/local/bin/kind "https://kind.sigs.k8s.io/dl/v${KIND_VERSION}/kind-${OS}-${ARCH}"
 chmod 0755 /usr/local/bin/kind
 
 echo "Install manifest-tool"
-MANIFEST_VERSION=$(curl https://api.github.com/repos/estesp/manifest-tool/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-curl -Lo /tmp/binaries-manifest-tool.tar.gz "https://github.com/estesp/manifest-tool/releases/download/v${MANIFEST_VERSION}/binaries-manifest-tool-${MANIFEST_VERSION}.tar.gz"
+MANIFEST_VERSION=$(curl -sL https://api.github.com/repos/estesp/manifest-tool/releases/latest | jq -r .tag_name | sed -E 's/v(.*)/\1/')
+curl -sL --fail --show-error -o /tmp/binaries-manifest-tool.tar.gz "https://github.com/estesp/manifest-tool/releases/download/v${MANIFEST_VERSION}/binaries-manifest-tool-${MANIFEST_VERSION}.tar.gz"
 tar -zxf /tmp/binaries-manifest-tool.tar.gz "manifest-tool-${OS}-${ARCH}"
 mv "manifest-tool-${OS}-${ARCH}" "/usr/local/bin/manifest-tool"
 chmod 0755 /usr/local/bin/manifest-tool
@@ -143,13 +152,13 @@ rm -rf /tmp/binaries-manifest-tool.tar.gz
 
 echo "Install grype"
 GRYPE_VERSION=$(curl -sL "https://api.github.com/repos/anchore/grype/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-curl -OL "https://github.com/anchore/grype/releases/download/v${GRYPE_VERSION}/grype_${GRYPE_VERSION}_${OS}_${ARCH}.deb"
+curl -sLO --fail --show-error "https://github.com/anchore/grype/releases/download/v${GRYPE_VERSION}/grype_${GRYPE_VERSION}_${OS}_${ARCH}.deb"
 apt install "./grype_${GRYPE_VERSION}_${OS}_${ARCH}.deb"
 rm "./grype_${GRYPE_VERSION}_${OS}_${ARCH}.deb"
 
 echo "Install syft"
 SYFT_VERSION=$(curl -sL "https://api.github.com/repos/anchore/syft/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-curl -OL "https://github.com/anchore/syft/releases/download/v${SYFT_VERSION}/syft_${SYFT_VERSION}_${OS}_${ARCH}.deb"
+curl -sLO --fail --show-error "https://github.com/anchore/syft/releases/download/v${SYFT_VERSION}/syft_${SYFT_VERSION}_${OS}_${ARCH}.deb"
 apt install "./syft_${SYFT_VERSION}_${OS}_${ARCH}.deb"
 rm "./syft_${SYFT_VERSION}_${OS}_${ARCH}.deb"
 
@@ -157,9 +166,11 @@ echo "install testssl.sh"
 git clone --depth 1 https://github.com/drwetter/testssl.sh.git /usr/local/testssl.sh
 chmod 0755 /usr/local/testssl.sh
 
-echo "Install Ansible and ansible-modules-hashivault"
-apt-get install -y --no-install-recommends python3-pip python3-venv twine python3-docker python3-psycopg2 postgresql-client-15
-PIP_BREAK_SYSTEM_PACKAGES=1 pip3 install --no-cache-dir ansible ansible-modules-hashivault tox virtualenv twine passlib ansible-lint checkov opensearch-py
+echo "Install Postgresql client"
+curl -sL --fail --show-error https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor > /usr/share/keyrings/postgresql.gpg
+echo "deb [signed-by=/usr/share/keyrings/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | tee -a /etc/apt/sources.list.d/pgdg.list
+apt-get update >/dev/null
+apt-get install -y postgresql-client
 
 wget https://gitlab.com/gitlab-org/terraform-images/-/raw/master/src/bin/gitlab-terraform.sh -O /usr/bin/gitlab-terraform
 chmod +x /usr/bin/gitlab-terraform
